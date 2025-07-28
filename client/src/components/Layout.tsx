d7db70c8849a3682881d863fee77f2ffc67da162
Luciano Brum <sbrum.luciano@gmail.com>
2025-07-28 17:56:08 -0300

    Home pre-beta 0.0.1

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -4,17 +4,17 @@ import Footer from './Footer';
 import type { ReactNode } from 'react';
 
 type LayoutProps = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', minWidth: '100vw', backgroundColor: '#dcd9c9' }}>
-      <Header />
-      <main style={{ flex: 1, padding: '1rem' }}>{children}</main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div style={{ backgroundColor: '#dcd9c9' }}>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </div>
+    );
 };
 
 export default Layout;
